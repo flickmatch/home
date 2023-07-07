1d93ee0742e529d790a1188b85a5147184cfe7e4
rohans42 <rohans@flickmatch.in>
2023-07-06 19:49:55 -0600

    Updates

---
+++ assets/index.cb0a1ba8.js
@@ -0,0 +1,1 @@
+import{M as e}from"./Meta.c82ed364.js";import{F as a,T as t}from"./App.701d2f21.js";import{B as i,F as o,j as r}from"./index.844b9c20.js";import"./index.module.d4c3b8d8.js";import"./index.aac4973a.js";function d(){return i(o,{children:[r(e,{title:"page 1"}),r(a,{children:r(t,{variant:"h3",children:"Page 1"})})]})}export{d as default};
