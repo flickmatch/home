9b1cc977ee73a4d5d5f5315a58fb1c7692e867b6
sam5102 <ansh102u@gmail.com>
2023-07-18 07:11:51 +0530

    improving mobile ui

--- react-fm/src/pages/Match-queues/queue.tsx
+++ react-fm/src/pages/Match-queues/queue.tsx
@@ -40,12 +40,12 @@ function MatchQueue() {
 
               <FlexBox style={{marginTop: 20, color: '#fff'}} sx={{ flexGrow: 1 }}>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} style={{marginLeft: 20}}>
-                  <Grid item xs={2} sm={4} md={4}>
+                  <Grid item xs={4} sm={4} md={4}>
                     <Typography id="title">
                       Price <span><CurrencyRupeeSharp style={{fontSize: 22, marginBottom: -4}}/>200</span>
                     </Typography>
                   </Grid>
-                  <Grid item xs={2} sm={4} md={4}>
+                  <Grid item xs={4} sm={4} md={4}>
                     <Typography id="title">
                       Date <span>16/07/2023 8PM-9PM</span>
                     </Typography>
@@ -55,17 +55,17 @@ function MatchQueue() {
 
               <FlexBox style={{marginTop: 25, color: '#fff'}} sx={{ flexGrow: 1 }}>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} style={{marginLeft: 20, alignItems: 'center', marginBottom: 10}}>
-                  <Grid item xs={2} sm={4} md={4}>
+                  <Grid item xs={4} sm={4} md={4}>
                     <Typography id="title">
                       Google Map <LocationOnIcon style={{marginLeft: 15, marginBottom: -3, color: '#fff'}}/>
                     </Typography>
                   </Grid>
-                  <Grid item xs={2} sm={4} md={4}>
+                  <Grid item xs={4} sm={4} md={4}>
                     <Typography id="title">
                         Number of Players <span>14</span>
                     </Typography>
                   </Grid>
-                  <Grid item xs={2} sm={4} md={4}>
+                  <Grid item xs={4} sm={4} md={4}>
                     <Typography id="title">
                         Players Required <span>2</span>
                     </Typography>
@@ -76,24 +76,24 @@ function MatchQueue() {
           </AccordionSummary>
 
           <AccordionDetails>
-            <Box style={{marginTop: 20, marginLeft: 25}} sx={{ flexGrow: 1 }}>
+            <Box id='box' sx={{ flexGrow: 1 }}>
                 <Typography id="reserved">
                       Reserved Players
                 </Typography> 
               <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                  <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
+                  <Grid item xs={2} sm={4} md={4} id='grid'>
                     <Avatar id="avatar" alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1gptEOLWU_ZFZQGMwF_EzTYAvGmeerm5aqZJG9hnWAA&s" />
                     <Typography id="names">
                       Abhishek
                     </Typography>
                   </Grid>
-                  <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
+                  <Grid item xs={2} sm={4} md={4} id='grid'>
                     <Avatar id="avatar" alt="profile" src="https://pbs.twimg.com/media/FoUoGo3XsAMEPFr?format=jpg&name=4096x4096" />
                     <Typography id="names">
                       Uday
                     </Typography>
                   </Grid>
-                  <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
+                  <Grid item xs={2} sm={4} md={4} id='grid'>
                     <Avatar id="avatar" alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIYZ5Ug1CDMppuput1FGd-kwYNoZwcJwDY2fCPmlsK8Q&s" />
                     <Typography id="names">
                       Nishant
@@ -102,22 +102,22 @@ function MatchQueue() {
                 </Grid>
             </Box>
 
-            <Box style={{marginTop: 20, marginLeft: 25}} sx={{ flexGrow: 1 }}>
+            <Box id='box' sx={{ flexGrow: 1 }}>
               <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                  <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp1f10Ha21IUV3t0oABQAcNI1ncGs5o3ohWA&usqp=CAU" />
+                  <Grid item xs={2} sm={4} md={4} id='grid'>
+                    <Avatar id="avatar" alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp1f10Ha21IUV3t0oABQAcNI1ncGs5o3ohWA&usqp=CAU" />
                     <Typography id="names">
                       Shubham
                     </Typography>
                   </Grid>
-                  <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1OmtCTmW8kQi4I5LnRFkvVIhdKIs2RSnVTQ&usqp=CAU" />
+                  <Grid item xs={2} sm={4} md={4} id='grid'>
+                    <Avatar id="avatar" alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1OmtCTmW8kQi4I5LnRFkvVIhdKIs2RSnVTQ&usqp=CAU" />
                     <Typography id="names">
                       Abhinav
                     </Typography>
                   </Grid>
-                  <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://cdn.futbin.com/content/fifa19/img/players/p246525.png?v=23" />
+                  <Grid item xs={2} sm={4} md={4} id='grid'>
+                    <Avatar id="avatar" alt="profile" src="https://cdn.futbin.com/content/fifa19/img/players/p246525.png?v=23" />
                     <Typography id="names">
                       Vaibhav
                     </Typography>
@@ -125,21 +125,21 @@ function MatchQueue() {
                 </Grid>
             </Box>
 
-            <Box style={{marginTop: 20, marginLeft: 25}} sx={{ flexGrow: 1 }}>
+            <Box id='box' sx={{ flexGrow: 1 }}>
               <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                  <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://i.pinimg.com/originals/9a/23/b0/9a23b044d3ae8bb33087200aaf983c15.jpg" />
+                  <Grid item xs={2} sm={4} md={4} id='grid'>
+                    <Avatar id="avatar" alt="profile" src="https://i.pinimg.com/originals/9a/23/b0/9a23b044d3ae8bb33087200aaf983c15.jpg" />
                     <Typography id="names">
                       Harsh
                     </Typography>
                   </Grid>
-                  <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
+                  <Grid item xs={2} sm={4} md={4} id='grid'>
                     <AccountCircleIcon style={{marginRight: 15, fontSize: 42}}/>
                     <Typography id="names">
                       (Empty)
                     </Typography>
                   </Grid>
-                  <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
+                  <Grid item xs={2} sm={4} md={4} id='grid'>
                     <AccountCircleIcon style={{marginRight: 15, fontSize: 42}}/>
                     <Typography id="names">
                       (Empty)
@@ -148,25 +148,25 @@ function MatchQueue() {
                 </Grid>
             </Box>
             
-            <Box style={{marginTop: 20, marginLeft: 25}} sx={{ flexGrow: 1 }}>
+            <Box id='box' sx={{ flexGrow: 1 }}>
                 <Typography id="unreserved">
                       Un Reserved Players
                 </Typography> 
               <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                  <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://i.pinimg.com/736x/f0/8e/1e/f08e1ec7ab1589419d515b7fbb60d88b.jpg" />
+                  <Grid item xs={2} sm={4} md={4} id='grid'>
+                    <Avatar id="avatar" alt="profile" src="https://i.pinimg.com/736x/f0/8e/1e/f08e1ec7ab1589419d515b7fbb60d88b.jpg" />
                     <Typography id="names">
                       Sahil
                     </Typography>
                   </Grid>
-                  <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://cdn.futbin.com/content/fifa23/img/players/p50529093.png" />
+                  <Grid item xs={2} sm={4} md={4} id='grid'>
+                    <Avatar id="avatar" alt="profile" src="https://cdn.futbin.com/content/fifa23/img/players/p50529093.png" />
                     <Typography id="names">
                       Shubham
                     </Typography>
                   </Grid>
-                  <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://e0.pxfuel.com/wallpapers/762/930/desktop-wallpaper-allison-becker-baseball-glove-sports-jersey-thumbnail.jpg" />
+                  <Grid item xs={2} sm={4} md={4} id='grid'>
+                    <Avatar id="avatar" alt="profile" src="https://e0.pxfuel.com/wallpapers/762/930/desktop-wallpaper-allison-becker-baseball-glove-sports-jersey-thumbnail.jpg" />
                     <Typography id="names">
                       Gourav
                     </Typography>
@@ -237,19 +237,19 @@ function MatchQueue() {
                 </Typography> 
               <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                   <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1gptEOLWU_ZFZQGMwF_EzTYAvGmeerm5aqZJG9hnWAA&s" />
+                    <Avatar id="avatar" alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1gptEOLWU_ZFZQGMwF_EzTYAvGmeerm5aqZJG9hnWAA&s" />
                     <Typography style={{color: '#fff', fontSize: 16, fontWeight: 'bold'}}>
                       Abhishek
                     </Typography>
                   </Grid>
                   <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://pbs.twimg.com/media/FoUoGo3XsAMEPFr?format=jpg&name=4096x4096" />
+                    <Avatar id="avatar" alt="profile" src="https://pbs.twimg.com/media/FoUoGo3XsAMEPFr?format=jpg&name=4096x4096" />
                     <Typography style={{color: '#fff', fontSize: 16, fontWeight: 'bold'}}>
                       Uday
                     </Typography>
                   </Grid>
                   <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIYZ5Ug1CDMppuput1FGd-kwYNoZwcJwDY2fCPmlsK8Q&s" />
+                    <Avatar id="avatar" alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIYZ5Ug1CDMppuput1FGd-kwYNoZwcJwDY2fCPmlsK8Q&s" />
                     <Typography style={{color: '#fff', fontSize: 16, fontWeight: 'bold'}}>
                       Nishant
                     </Typography>
@@ -260,19 +260,19 @@ function MatchQueue() {
             <Box style={{marginTop: 20, marginLeft: 25}} sx={{ flexGrow: 1 }}>
               <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                   <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp1f10Ha21IUV3t0oABQAcNI1ncGs5o3ohWA&usqp=CAU" />
+                    <Avatar id="avatar" alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp1f10Ha21IUV3t0oABQAcNI1ncGs5o3ohWA&usqp=CAU" />
                     <Typography style={{color: '#fff', fontSize: 16, fontWeight: 'bold'}}>
                       Shubham
                     </Typography>
                   </Grid>
                   <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1OmtCTmW8kQi4I5LnRFkvVIhdKIs2RSnVTQ&usqp=CAU" />
+                    <Avatar id="avatar" alt="profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1OmtCTmW8kQi4I5LnRFkvVIhdKIs2RSnVTQ&usqp=CAU" />
                     <Typography style={{color: '#fff', fontSize: 16, fontWeight: 'bold'}}>
                       Abhinav
                     </Typography>
                   </Grid>
                   <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://cdn.futbin.com/content/fifa19/img/players/p246525.png?v=23" />
+                    <Avatar id="avatar" alt="profile" src="https://cdn.futbin.com/content/fifa19/img/players/p246525.png?v=23" />
                     <Typography style={{color: '#fff', fontSize: 16, fontWeight: 'bold'}}>
                       Vaibhav
                     </Typography>
@@ -283,7 +283,7 @@ function MatchQueue() {
             <Box style={{marginTop: 20, marginLeft: 25}} sx={{ flexGrow: 1 }}>
               <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                   <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://i.pinimg.com/originals/9a/23/b0/9a23b044d3ae8bb33087200aaf983c15.jpg" />
+                    <Avatar id="avatar" alt="profile" src="https://i.pinimg.com/originals/9a/23/b0/9a23b044d3ae8bb33087200aaf983c15.jpg" />
                     <Typography style={{color: '#fff', fontSize: 16, fontWeight: 'bold'}}>
                       Harsh
                     </Typography>
@@ -309,19 +309,19 @@ function MatchQueue() {
                 </Typography> 
               <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                   <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://i.pinimg.com/736x/f0/8e/1e/f08e1ec7ab1589419d515b7fbb60d88b.jpg" />
+                    <Avatar id="avatar" alt="profile" src="https://i.pinimg.com/736x/f0/8e/1e/f08e1ec7ab1589419d515b7fbb60d88b.jpg" />
                     <Typography style={{color: '#fff', fontSize: 16, fontWeight: 'bold'}}>
                       Sahil
                     </Typography>
                   </Grid>
                   <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://cdn.futbin.com/content/fifa23/img/players/p50529093.png" />
+                    <Avatar id="avatar" alt="profile" src="https://cdn.futbin.com/content/fifa23/img/players/p50529093.png" />
                     <Typography style={{color: '#fff', fontSize: 16, fontWeight: 'bold'}}>
                       Shubham
                     </Typography>
                   </Grid>
                   <Grid item xs={2} sm={4} md={4} style={{display: 'flex', alignItems: 'center'}}>
-                    <Avatar style={{marginRight: 15}} alt="profile" src="https://e0.pxfuel.com/wallpapers/762/930/desktop-wallpaper-allison-becker-baseball-glove-sports-jersey-thumbnail.jpg" />
+                    <Avatar id="avatar" alt="profile" src="https://e0.pxfuel.com/wallpapers/762/930/desktop-wallpaper-allison-becker-baseball-glove-sports-jersey-thumbnail.jpg" />
                     <Typography style={{color: '#fff', fontSize: 16, fontWeight: 'bold'}}>
                       Gourav
                     </Typography>
