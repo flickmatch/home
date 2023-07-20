d4a9f0ac719c130b115a92551cb596e29eb5f117
rohans42 <rohans@flickmatch.in>
2023-07-20 10:29:11 -0600

    Updates

--- assets/index.8b708b8e.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./Meta.6477965f.js";import{F as a,T as t}from"./App.3ee230da.js";import{B as i,F as o,j as r}from"./index.844b9c20.js";import"./index.module.d4c3b8d8.js";import"./index.2a14fe8c.js";function d(){return i(o,{children:[r(e,{title:"page 3"}),r(a,{children:r(t,{variant:"h3",children:"Page 3"})})]})}export{d as default};
