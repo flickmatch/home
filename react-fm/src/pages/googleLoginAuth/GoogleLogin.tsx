89be654265bc49693487c9cfbacdc5f6c10f30bb
sam5102 <ansh102u@gmail.com>
2023-12-21 20:54:02 +0530

    Enhanced login ui

--- react-fm/src/pages/googleLoginAuth/GoogleLogin.tsx
+++ react-fm/src/pages/googleLoginAuth/GoogleLogin.tsx
@@ -1,19 +1,20 @@
 import GoogleIcon from '@mui/icons-material/Google';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
 import { Button } from '@mui/material';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 import Meta from '@/components/Meta';
 import useOrientation from '@/hooks/useOrientation';
 
 import styles from './GoogleLogin.module.scss';
-import platformLogo from '/logo.png';
 
 function GoogleLogin() {
   const isPortrait = useOrientation();
 
   return (
     <>
-      <Meta title="Contact Us" />
+      <Meta title="Login/Signup" />
       <Box className={styles.container}>
         <Box className={isPortrait ? styles.portraitLeftSide : styles.leftSide}>
           <img
@@ -24,18 +25,26 @@ function GoogleLogin() {
 
         <Box className={isPortrait ? styles.portraitRightSide : styles.rightSide}>
           <Box className={isPortrait ? styles.portraitLoginContainer : styles.loginContainer}>
-            <Box className={styles.logo}>
-              <img src={platformLogo} alt="Matchmaking Platform Logo" />
+            <Box className={styles.sportsIconSection}>
+              <SportsSoccerIcon className={styles.sportsIcon} />
             </Box>
-
-            <Button
-              variant="outlined"
-              className={isPortrait ? styles.portraitGoogleLoginButton : styles.googleLoginButton}
-              onClick={() => alert('Logged In')}
-              startIcon={<GoogleIcon />}
-            >
-              Login with Google
-            </Button>
+            <Box className={styles.loginSignupHeader}>
+              <Typography className={styles.loginSignupText}>Login/Signup</Typography>
+              <Typography className={styles.getStarted}>Let&#39;s get Started</Typography>
+              <Typography className={styles.signUpImmediately}>
+                Join our sports organizing community and start your journey right away!
+              </Typography>
+            </Box>
+            <Box className={styles.loginSignupButton}>
+              <Button
+                variant="outlined"
+                className={isPortrait ? styles.portraitGoogleLoginButton : styles.googleLoginButton}
+                onClick={() => alert('Logged In')}
+                startIcon={<GoogleIcon />}
+              >
+                Sign up with Google
+              </Button>
+            </Box>
           </Box>
         </Box>
       </Box>
