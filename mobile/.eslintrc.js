71bdae4942c74679adf922611bca66e8c9dae3f2
Kshitij <kshitij@flickmatch.in>
2024-03-03 10:04:23 +0530

    Added eslint and prettier configuration for the project.

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      2,
+      {extensions: ['.js', '.jsx', '.ts', '.tsx']},
+    ],
+  },
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended'],
 };
