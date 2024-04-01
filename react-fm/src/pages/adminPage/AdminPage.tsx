2a61de8a94ca8aaf5718ee67784bc448c8d394db
Sumit Aggarwal <sumitaggarwal@Sumits-MacBook-Air.local>
2024-04-01 20:56:35 +0530

    swal build fail issue

--- react-fm/src/pages/adminPage/AdminPage.tsx
+++ react-fm/src/pages/adminPage/AdminPage.tsx
@@ -75,7 +75,6 @@ function AdminPage() {
       fetchMailIds();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
