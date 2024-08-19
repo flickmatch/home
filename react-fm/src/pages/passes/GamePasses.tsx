124197947853371795e4880d5d4eaaef759eb0a3
Sumit Aggarwal <sumitaggarwal@Sumits-MacBook-Air.local>
2024-08-19 11:07:06 +0530

    added FPL link in rewards

--- react-fm/src/pages/passes/GamePasses.tsx
+++ react-fm/src/pages/passes/GamePasses.tsx
@@ -53,7 +53,7 @@ function GamePasses() {
           <Typography variant="h3" className={styles.title}>
             10 games pass
           </Typography>
-          <Typography className={styles.subtitle}>pay for 8 games, play 10 games</Typography>
+          <Typography className={styles.subtitle}>Pay for 8 games, Play 10 games</Typography>
           <Typography className={styles.price}>
             <CurrencyRupeeIcon className={styles.currency} />
             3000
@@ -76,18 +76,37 @@ function GamePasses() {
           </ul>
           <Button className={styles.buyNow}>Buy Now</Button>
         </Box>
-        {/* <Box className={styles.passCard}>
+        <Box className={isPortrait ? styles.portraitPassCard : styles.passCard}>
+          <img src={appLogo} alt="logo" className={styles.logo} />
           <Typography variant="h3" className={styles.title}>
-            30 days pass
+            FPL Reward
           </Typography>
-          <Typography className={styles.subtitle}>Unlimited games for 1 month</Typography>
-          <Typography className={styles.price}>
-            <CurrencyRupeeIcon className={styles.currency} />
-            1000
+          <Typography className={styles.subtitle}>
+            Fantasy Premier League winner gets a free game!
           </Typography>
-          <Typography className={styles.status}>Active</Typography>
-          <Button className={styles.buyNow}>Buy Now</Button>
-        </Box> */}
+          <Typography className={styles.price}>Free</Typography>
+          <Typography className={styles.status}>LIMITED OFFER</Typography>
+
+          <ul className={styles.details}>
+            <li>
+              <SportsSoccerIcon className={styles.footballIcon} />1 free game at any location
+            </li>
+            <li>
+              <SportsSoccerIcon className={styles.footballIcon} />
+              Exclusive for the top winner
+            </li>
+            <li>
+              <SportsSoccerIcon className={styles.footballIcon} />
+              Valid for 1 week
+            </li>
+          </ul>
+          <Button
+            className={styles.buyNow}
+            href="https://fantasy.premierleague.com/leagues/auto-join/nl0zon"
+          >
+            Join FPL
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
