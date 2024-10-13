e8f076296950bcbaa5cc376a36fdbfe7d1c46f85
abhimanyu-fm <abhimanyu-fm@users.noreply.github.com>
2024-10-13 09:02:42 +0000

    deploy: 75c4920aeab1d37deae38b331ee60ab6e564bb42

---
+++ assets/index.3b352db4.js
@@ -0,0 +1,35 @@
+import{W as J,a as o,K as i,M as y,j as t}from"./loginSlice.25a195ef.js";import{S as U,d as z}from"./sweetalert2.all.22b6e245.js";import{d as H}from"./CrisisAlertOutlined.fd13e4ec.js";import{bt as Z,u as K,bu as Q,F as v,N as Y,B as r,T as m,bB as R,b as X}from"./App.3c90ebfd.js";import{M as ee}from"./Meta.32417e50.js";import{q as te}from"./constants.70367cb1.js";import{Z as se}from"./Zoom.899b1154.js";import{a as ie,S as oe,T as I}from"./TextField.357fde03.js";import{M as ae,A as T,a as g}from"./MenuItem.9758472f.js";import"./index.988de928.js";import"./index.module.51bb9f99.js";import"./Input.a233619a.js";const ne="_container_16cp1_1",re="_portraitContainer_16cp1_2",ce="_adminModeTag_16cp1_23",le="_title_16cp1_30",de="_divider_16cp1_38",pe="_fieldTitle_16cp1_46",ue="_fieldTitleIcon_16cp1_52",me="_sectionFirst_16cp1_59",fe="_sectionSecond_16cp1_60",he="_sectionThird_16cp1_61",ye="_formSection_16cp1_65",Te="_selectInput_16cp1_71",ge="_sectionFourth_16cp1_79",Ne="_createButton_16cp1_86",s={container:ne,portraitContainer:re,adminModeTag:ce,title:le,divider:de,fieldTitle:pe,fieldTitleIcon:ue,sectionFirst:me,sectionSecond:fe,sectionThird:he,formSection:ye,selectInput:Te,sectionFourth:ge,createButton:Ne},_e="https://service.flickmatch.in/platform-0.0.1-SNAPSHOT/graphql",C="https://service.flickmatch.in:8443/platform-0.0.1-SNAPSHOT/graphql";JSON.stringify({query:`query {
+        city(cityId) {
+          cityId
+          cityName
+          sportsVenues {
+            sportsVenueId
+            displayName
+            googleMapsLink
+            availableSportsIds
+          }      
+        }
+      }`});function qe(){const[,f]=Z(),M=K(),h=Q(),d=J(e=>e),[c,x]=o.exports.useState(""),[a,N]=o.exports.useState(""),[p,_]=o.exports.useState(""),[A,b]=o.exports.useState([]),[w,F]=o.exports.useState([]);o.exports.useEffect(()=>{d.login.isAdmin||U.fire({title:"Unauthorized Access",text:"You are not authorized to view this page.",icon:"error"}).then(()=>{h("/match-queues")})},[d.login.isAdmin,h]),o.exports.useEffect(()=>{const e=new AbortController,u=e.signal;return(async()=>{try{const G=await(await fetch(C,{method:"POST",signal:u,headers:{"Content-Type":"application/json"},body:te})).json();b(G.data.cities)}catch(n){n instanceof Error&&n.name==="TypeError"&&console.log(n.message)}})(),()=>{e.abort()}},[]);function k(){f.push({options:{content:t(T,{severity:"success",children:t(g,{className:s.alertTitle,children:"Turf Added Successfully"})})}})}function L(){f.push({options:{content:t(T,{severity:"info",children:i(g,{className:s.alertTitle,children:[c===""?"City Name":a===""?"Turf Name":"Google Map field"," ","cannot be empty!"]})})}})}function S(e){f.push({options:{content:t(T,{severity:"error",children:t(g,{className:s.alertTitle,children:e})})}})}const q=e=>{(async()=>{try{const n=await(await fetch(C,{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({query:`query {
+              city(cityId: "${e}") {
+                cityId
+                cityName
+                sportsVenues {
+                  sportsVenueId
+                  displayName
+                  googleMapsLink
+                  availableSportsIds
+                }      
+              }
+            }`})})).json();F(n.data.city.sportsVenues)}catch(l){l instanceof Error&&l.name==="TypeError"&&console.log(l.message)}})()},j=e=>{x(e.target.value),q(e.target.value)},O=e=>{N(e.target.value)},B=e=>{_(e.target.value)},P={input:{cityId:c,displayName:a,googleMapsLink:p,stripePaymentLinks:[]}},E=()=>{if(c===""||p===""||a==="")L();else{if(w.some(e=>Object.values(e).includes(a)))return S("Turf already exists!");fetch(_e,{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({query:`mutation {
+              createSportsVenue(
+                    input: {
+                        cityId: "${c}"
+                        displayName: "${a}"
+                        googleMapsLink: "${p}"
+                        stripePaymentLinks: []
+                    }
+                ) {
+                  isSuccessful
+                  errorMessage
+                }
+            }`,variables:P.input})}).then(e=>e.json()).then(e=>{if(e.errors)throw new Error(e.errors[0].message);k(),N(""),_(""),h("/match-queues")}).catch(e=>{S(e.message),console.log(e)})}},V=()=>i(r,{className:s.sectionFirst,children:[t(m,{className:s.fieldTitle,children:"City Name"}),t(z,{className:s.fieldTitleIcon}),t(ie,{className:s.selectInput,children:t(oe,{id:"demo-simple-select-autowidth",value:c,onChange:j,style:{width:"100%"},children:A.map((e,u)=>t(ae,{value:e.cityId,style:{width:"100%"},children:e.cityName},u))})})]}),D=()=>i(r,{className:s.sectionSecond,children:[t(m,{className:s.fieldTitle,children:"Turf Name"}),t(R,{className:s.fieldTitleIcon}),t(I,{fullWidth:!0,value:a,onChange:O,placeholder:"Turf Name",id:"fullWidth"})]}),$=()=>i(r,{className:s.sectionThird,children:[t(m,{className:s.fieldTitle,children:"Google Map Link"}),t(H,{className:s.fieldTitleIcon}),t(I,{fullWidth:!0,value:p,onChange:B,placeholder:"G-Map Link",id:"fullWidth"})]}),W=()=>t(r,{className:s.sectionFourth,children:t(X,{variant:"contained",color:"success",className:s.createButton,onClick:E,children:"Create"})});return i(y,{children:[t(ee,{title:"Add Turf"}),d.login.isAdmin&&d.login.isLoggedIn?t(y,{children:t(se,{in:!0,style:{transitionDelay:"300ms"},children:i(v,{className:M?s.portraitContainer:s.container,children:[t(Y,{label:"admin mode",color:"primary",variant:"outlined",className:s.adminModeTag}),i(r,{children:[t(m,{variant:"h3",className:s.title,children:"Add Turf"}),t(r,{className:s.divider})]}),i(v,{className:s.formSection,children:[V(),D(),$(),W()]})]})})}):t(y,{})]})}export{qe as default};
---
+++ assets/index.dbab4061.js
@@ -0,0 +1,38 @@
+import{a as p,W as R,K as i,M as S,j as a}from"./loginSlice.25a195ef.js";import{r as x,i as O,a as T,u as E,B as f,T as d,d as I,b as $}from"./App.3c90ebfd.js";import{M as H}from"./Meta.32417e50.js";import{m as U}from"./map.839e9770.js";import{Z as A}from"./Zoom.899b1154.js";import"./index.988de928.js";import"./index.module.51bb9f99.js";var z={},B=O.exports;Object.defineProperty(z,"__esModule",{value:!0});var w=z.default=void 0,F=B(x()),G=T,M=(0,F.default)((0,G.jsx)("path",{d:"M13.66 7c-.56-1.18-1.76-2-3.16-2H6V3h12v2h-3.26c.48.58.84 1.26 1.05 2H18v2h-2.02c-.25 2.8-2.61 5-5.48 5h-.73l6.73 7h-2.77L7 14v-2h3.5c1.76 0 3.22-1.3 3.46-3H6V7h7.66z"}),"CurrencyRupee");w=z.default=M;const J="https://firebasestorage.googleapis.com/v0/b/flickmatch-374a2.appspot.com/o/fm_rainbow.png?alt=media&token=1b06ae27-bf10-4974-9100-6bb5f2308314",L="_logo_rzs7q_19",K="_heading_rzs7q_24",V="_pageContainer_rzs7q_29",W="_container_rzs7q_34",X="_passCard_rzs7q_44",Z="_portraitPassCard_rzs7q_45",Q="_ribbon_rzs7q_63",Y="_title_rzs7q_112",ee="_subtitle_rzs7q_121",te="_price_rzs7q_126",ae="_currency_rzs7q_134",se="_status_rzs7q_140",oe="_pulse_rzs7q_1",re="_portraitDetails_rzs7q_156",ie="_details_rzs7q_165",ne="_sportsIcon_rzs7q_192",ce="_buyNow_rzs7q_207",le="_footballIcon_rzs7q_232",pe="_cityNameContainer_rzs7q_238",me="_citiesName_rzs7q_244",de="_citiesImg_rzs7q_254",t={logo:L,heading:K,pageContainer:V,container:W,passCard:X,portraitPassCard:Z,ribbon:Q,title:Y,subtitle:ee,price:te,currency:ae,status:se,pulse:oe,portraitDetails:re,details:ie,sportsIcon:ne,buyNow:ce,footballIcon:le,cityNameContainer:pe,citiesName:me,citiesImg:de},ue=(s,n,u,l,y)=>fetch("https://service.flickmatch.in/platform-0.0.1-SNAPSHOT/graphql",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({query:`mutation InitiatePassPayment {
+            initiatePassPayment(
+                input: {
+                    passId: "${s}"
+                    email: "${n}",
+                    currency: "${y}",
+                    location: "${l}"
+                }
+            ) {
+                orderId
+                isInitiated
+                amount
+            }
+        }`})}).then(o=>o.json()).then(o=>{if(o.errors)throw new Error(o.errors[0].message);return u(o.data.initiatePassPayment.amount),o.data.initiatePassPayment.orderId}).catch(o=>{console.log(o)});function ye(){return new Promise(s=>{const n=document.createElement("script");n.src="https://checkout.razorpay.com/v1/checkout.js",n.onload=()=>{s(!0)},n.onerror=()=>{s(!1)},document.body.appendChild(n)})}function he(){const s=document.querySelector('script[src="https://checkout.razorpay.com/v1/checkout.js"]');s&&document.body.removeChild(s)}const _e=(s,n,u,l,y,o,g)=>{async function b(){await ye()||alert("Razorpay SDK failed to load.Please try again later.");const _={key:"rzp_live_ba3UQjRIBXdeXt",amount:s,currency:"INR",name:"Flickmatch",description:"Flickmatch transaction for football match.",image:"https://firebasestorage.googleapis.com/v0/b/flickmatch-374a2.appspot.com/o/fm_rainbow.png?alt=media&token=1b06ae27-bf10-4974-9100-6bb5f2308314",order_id:n,callback_url:"https://service.flickmatch.in/platform-0.0.1-SNAPSHOT/processPassPayment",redirect:!0,prefill:{name:l,email:u,contact:y},notes:{address:"Tower 8 - Flat 1501, Nirvana Country Rd, The Close South, Sector 50, Gurugram, Haryana 122018"},modal:{ondismiss:function(){g(!1),o(""),he(),delete h.Razorpay}},theme:{color:"#4ce95a"}},h=window;new h.Razorpay(_).open()}b()},fe=JSON.stringify({query:`query Passes {
+  passes {
+  passId
+        cityId
+        passType
+        totalGames
+        totalDays
+        price
+        title
+        status
+  }
+    }`}),D="https://service.flickmatch.in/platform-0.0.1-SNAPSHOT/graphql";function Ce(){const s=E(),[n,u]=p.exports.useState([]),[l,y]=p.exports.useState({name:"",email:"",phoneNumber:""}),[o,g]=p.exports.useState(!0),[b,q]=p.exports.useState(0),[_,h]=p.exports.useState(""),N=R(e=>e);p.exports.useEffect(()=>{const e=localStorage.getItem("userData");if(e){const r=JSON.parse(e);y({name:r.name,email:r.email,phoneNumber:""})}},[]),p.exports.useEffect(()=>{(async()=>{try{const m=(await(await fetch(D,{method:"POST",headers:{"Content-Type":"application/json"},body:fe})).json()).data.passes,c=[];m.forEach(C=>{U.forEach(P=>{if(P.cityId===Number(C.cityId)){const j={cityDetails:P,pass:C};c.push(j)}})}),u(c),console.log(c)}catch(r){r instanceof Error&&r.name==="TypeError"&&console.log(r)}})()},[]);const k=(e,r,v)=>{o?ue(e,l.email,q,r,v).then(m=>{h(m)}).catch(m=>{console.error(m)}):fetch(D,{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({query:`mutation InitiatePassPayment {
+            initiatePassPayment(
+                input: {
+                    passId: "${e}"
+                    email: "${l.email}",
+                    currency: "INR",
+                    location: "${r}"
+                }
+            ) {
+                orderId
+                isInitiated
+                amount
+            }
+        }`})}).then(c=>c.json()).then(c=>{if(c.errors)throw new Error(c.errors[0].message)}).catch(c=>{console.log(c)})};return _&&_e(b,_,l.email,l.name,l.phoneNumber,h,g),i(S,{children:[a(H,{title:"Game Passes"}),N.login.isAdmin&&N.login.isLoggedIn?a(A,{in:!0,style:{transitionDelay:"300ms"},children:i(f,{className:t.pageContainer,children:[a(d,{variant:"h3",className:t.heading,children:"Flickmatch Pass"}),a(f,{className:t.container,children:n.length>0?n.map((e,r)=>i(S,{children:[i("div",{className:t.cityNameContainer,children:[a(d,{className:t.citiesName,children:e.cityDetails.city},e.cityDetails.cityId),a("img",{className:t.citiesImg,src:e.cityDetails.iconUrl,alt:e.cityDetails.city,height:"40px"})]}),i(f,{className:s?t.portraitPassCard:t.passCard,children:[a("img",{src:J,alt:"logo",className:t.logo}),a(d,{variant:"h3",className:t.title,children:e.pass.title}),i(d,{className:t.subtitle,children:["Pay for ",e.pass.totalGames-2," games, Play"," ",e.pass.totalGames," games"]}),i(d,{className:t.price,children:[a(w,{className:t.currency}),e.pass.price]}),a(d,{className:t.status,children:e.pass.status}),i("ul",{className:s?t.portraitDetails:t.details,children:[i("li",{children:[a(I,{className:t.footballIcon}),"Access to all fields"]}),i("li",{children:[a(I,{className:t.footballIcon}),"Join game in single click"]}),i("li",{children:[a(I,{className:t.footballIcon}),"Exclusive member events"]})]}),a(f,{className:t.ribbon,children:e.pass.title==="Unlimited game pass"?a("span",{style:{fontWeight:600},children:"Best Deal"}):i("span",{children:["For ",e.pass.totalDays," Days"]})}),a($,{className:t.buyNow,onClick:()=>k(e.pass.passId,e.cityDetails.city,e.cityDetails.currency),children:"Buy Now"})]},r)]})):null})]})}):null]})}export{Ce as default};
