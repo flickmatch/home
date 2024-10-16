eead7652d1324d3fa357ba0419a6c2b3b7bb0cd4
Kshitij <guptakshitij2308@gmail.com>
2024-10-16 22:26:36 +0530

    Sanitized logs.

--- platform/src/main/java/com/flickmatch/platform/rest/RazorPaymentCallbackController.java
+++ platform/src/main/java/com/flickmatch/platform/rest/RazorPaymentCallbackController.java
@@ -157,9 +157,11 @@ public class RazorPaymentCallbackController {
 
     }
 
+
+
     @PostMapping("/razorpay-webhook")
     public ResponseEntity<Void> razorpayWebhook(@RequestBody(required = false) String payload) {
-        log.info("Razorpay Webhook received: {}", payload);
+        log.info("Razorpay Webhook received: {}", sanitizeForLog(payload));
         return ResponseEntity.ok().build();
     }
 
