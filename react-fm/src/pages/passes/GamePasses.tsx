a38f95fe68e4c6d644f1636b13f195e5ad20c6c3
Sumit Aggarwal <sumitaggarwal@Sumits-MacBook-Air.local>
2024-10-17 00:02:14 +0530

    final subscription changes

--- react-fm/src/pages/passes/GamePasses.tsx
+++ react-fm/src/pages/passes/GamePasses.tsx
@@ -185,7 +185,7 @@ function GamePasses() {
   return (
     <>
       <Meta title="Game Passes" />
-      {userState.login.isAdmin && userState.login.isLoggedIn ? (
+      {userState.login.isLoggedIn ? (
         <Zoom in={true} style={{ transitionDelay: '300ms' }}>
           <Box className={styles.pageContainer}>
             <Typography variant="h3" className={styles.heading}>
