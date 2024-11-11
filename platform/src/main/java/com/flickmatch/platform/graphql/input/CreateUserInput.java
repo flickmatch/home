55132cf7c749195eaa0527cd7619355256f0eafb
Kshitij <guptakshitij2308@gmail.com>
2024-11-12 01:04:32 +0530

    Added cities history to the user model.

--- platform/src/main/java/com/flickmatch/platform/graphql/input/CreateUserInput.java
+++ platform/src/main/java/com/flickmatch/platform/graphql/input/CreateUserInput.java
@@ -3,10 +3,13 @@ package com.flickmatch.platform.graphql.input;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Optional;
+
 @Getter
 @Builder
 public class CreateUserInput {
     String name;
     String email;
     String phoneNumber;
+    String location;
 }
