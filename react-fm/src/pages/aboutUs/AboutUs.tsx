0d88ecebbfc7af298905b62953c9d4c4f3f1a225
Sumit Aggarwal <sumitaggarwal@Sumits-MacBook-Air.local>
2024-12-08 20:22:45 +0530

    text changed  on about-us page / replace monthly passes to Gift passes

--- react-fm/src/pages/aboutUs/AboutUs.tsx
+++ react-fm/src/pages/aboutUs/AboutUs.tsx
@@ -17,20 +17,17 @@ function AboutUs() {
         <Typography variant="h3" className={styles.ourStory}>
           Our Story
         </Typography>
-        <Typography variant="h4" className={styles.journey}>
-          We&#39;re on a Journey
-        </Typography>
 
         <Typography className={isPortrait ? styles.mobileMotto : styles.motto}>
-          FlickMatch is envisioned to be a matchmaking, player record keeping and ranking platform
-          for casual sports in India and USA.
+          FlickMatch is a match making, player record keeping and ranking platform for casual sports
+          in India and USA.
         </Typography>
 
         <Typography className={isPortrait ? styles.mobileDescription : styles.description}>
-          While playing casual sports such as an evening game of football (soccer), an interested
-          person would have to find 13 other interested people and find a 7v7 ground. This is
-          tedious and de-motivational. We&#39;re a bunch of people building processes and technology
-          to enhance local sports.
+          While playing sports such as an evening game of football (soccer), an interested person
+          would have to find 13 other interested people and find a 7v7 ground. This is tedious and
+          de-motivational. We&#39;re a bunch of people building processes and technology to enhance
+          local sports.
         </Typography>
 
         <Typography className={isPortrait ? styles.mobileAim : styles.aim}>
