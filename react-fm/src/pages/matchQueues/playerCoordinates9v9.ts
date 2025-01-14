c53a9f1056f85c8c26613129b781c67ca9b663bf
Sumit Aggarwal <sumitaggarwal@Sumits-MacBook-Air.local>
2025-01-14 23:32:17 +0530

    formations positions aligned

--- react-fm/src/pages/matchQueues/playerCoordinates9v9.ts
+++ react-fm/src/pages/matchQueues/playerCoordinates9v9.ts
@@ -5,43 +5,43 @@ const teamACoordinates9 = [
     role: 'GK',
   },
   {
-    mobilePoints: { x: 12, y: 95 },
+    mobilePoints: { x: 8, y: 95 },
     id: 2,
     role: 'MF',
   },
   {
-    mobilePoints: { x: 44, y: 95 },
+    mobilePoints: { x: 45, y: 95 },
     id: 3,
     role: 'MF',
   },
   {
-    mobilePoints: { x: 66, y: 95 },
+    mobilePoints: { x: 80, y: 95 },
     id: 4,
     role: 'MF',
   },
   {
-    mobilePoints: { x: 12, y: 165 },
+    mobilePoints: { x: 8, y: 165 },
     id: 5,
     role: 'MF',
   },
   {
-    mobilePoints: { x: 44, y: 165 },
+    mobilePoints: { x: 45, y: 165 },
     id: 6,
     role: 'MD',
   },
   {
-    mobilePoints: { x: 76, y: 165 },
+    mobilePoints: { x: 80, y: 165 },
     id: 7,
     role: 'MD',
   },
 
   {
-    mobilePoints: { x: 29, y: 240 },
+    mobilePoints: { x: 23, y: 247 },
     id: 8,
     role: 'A',
   },
   {
-    mobilePoints: { x: 60, y: 240 },
+    mobilePoints: { x: 65, y: 247 },
     id: 9,
     role: 'A',
   },
@@ -49,48 +49,48 @@ const teamACoordinates9 = [
 
 const teamBCoordinates9 = [
   {
-    mobilePoints: { x: 29, y: 370 },
+    mobilePoints: { x: 23, y: 357 },
     id: 1,
     role: 'A',
   },
   {
-    mobilePoints: { x: 60, y: 370 },
+    mobilePoints: { x: 65, y: 357 },
     id: 2,
     role: 'A',
   },
 
   {
-    mobilePoints: { x: 12, y: 440 },
+    mobilePoints: { x: 8, y: 439 },
     id: 3,
     role: 'MD',
   },
   {
-    mobilePoints: { x: 44, y: 440 },
+    mobilePoints: { x: 45, y: 439 },
     id: 4,
     role: 'MD',
   },
   {
-    mobilePoints: { x: 76, y: 440 },
+    mobilePoints: { x: 80, y: 439 },
     id: 5,
     role: 'MF',
   },
   {
-    mobilePoints: { x: 12, y: 520 },
+    mobilePoints: { x: 8, y: 509 },
     id: 6,
     role: 'MF',
   },
   {
-    mobilePoints: { x: 44, y: 520 },
+    mobilePoints: { x: 45, y: 509 },
     id: 7,
     role: 'MF',
   },
   {
-    mobilePoints: { x: 76, y: 520 },
+    mobilePoints: { x: 80, y: 509 },
     id: 8,
     role: 'MF',
   },
   {
-    mobilePoints: { x: 45, y: 581 },
+    mobilePoints: { x: 45, y: 578 },
     id: 9,
     role: ' GK',
   },
