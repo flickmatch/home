211e98159ef44ab9c1858d109160d3f594fce2a9
Zeeshan <zeeshan2002>
2025-03-20 09:27:47 +0530

    Resolved and Merged conflicts

--- platform/src/main/java/com/flickmatch/platform/dynamodb/model/Event.java
+++ platform/src/main/java/com/flickmatch/platform/dynamodb/model/Event.java
@@ -85,6 +85,8 @@ public class Event {
         String team2Color;
 
         String currency;
+        Integer team1Score;
+        Integer team2Score;
         Integer downloadCounter;
         //TODO:Add status attribute
 
@@ -249,6 +251,25 @@ public class Event {
         public void setPlayerDetailsList(List<PlayerDetails> playerDetailsList) {
             this.playerDetailsList = playerDetailsList;
         }
+
+        @DynamoDBAttribute(attributeName = "team1Score")
+        public Integer getTeam1Score() {
+            return team1Score;
+        }
+
+        public void setTeam1Score(Integer team1Score) {
+            this.team1Score = team1Score;
+        }
+
+        @DynamoDBAttribute(attributeName = "team2Score")
+        public Integer getTeam2Score() {
+            return team2Score;
+        }
+
+        public void setTeam2Score(Integer team2Score) {
+            this.team2Score = team2Score;
+        }
+
         @DynamoDBAttribute(attributeName = "downloadCounter")
         public Integer getDownloadCounter() {
             return downloadCounter;
