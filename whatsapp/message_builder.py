4256587a24fd4d98ba7fbde7992687a2275fafe6
Kshitij <kshitij@flickmatch.in>
2025-05-07 00:45:17 +0530

    Added flask api implementation for twilio and refactored the code.

---
+++ whatsapp/message_builder.py
@@ -0,0 +1,31 @@
+from twilio.base.exceptions import TwilioRestException
+from twilio.rest.api.v2010.account.message import MessageInstance
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class MessageBuilder:
+    def __init__(self, twilio_proxy):
+        self.twilio_proxy = twilio_proxy
+
+    def send_message(self, from_number: str, to_number: str, message: str) -> str:
+        logger.info(f"Sending message from: {from_number} to: {to_number}")
+        try:
+            client = self.twilio_proxy.init_twilio()
+
+            message_response: MessageInstance = client.messages.create(
+                body=message,
+                from_=f"whatsapp:{from_number}",
+                to=f"whatsapp:{to_number}"
+            )
+
+            logger.info(
+                f"WhatsApp message sent successfully, SID: {message_response.sid}")
+            return message_response.sid
+        except TwilioRestException as e:
+            logger.error(f"Failed to send message: {str(e)}")
+            raise e
+        except Exception as e:
+            logger.error(f"Unexpected error: {str(e)}")
+            raise e
